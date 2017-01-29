d09d2877e946013845778b705a9fb577b6b8b691
Alessandro Marin <alessandro.m316@gmail.com>
2017-01-29 17:46:23 +0100

    makes vector return Nothing if the lenghts are declared as different)

--- chapter-2/Vector.hs
+++ chapter-2/Vector.hs
@@ -3,7 +3,8 @@ import Data.List
 -- Can be Vec Int or Vec Float
 data Vec t = Vec [t] Int deriving (Show, Eq, Ord)
 
-(<+>) :: Num t => Vec t -> Vec t -> Vec t
-(Vec v1 l1) <+> (Vec v2 l2) = Vec (zipWith (+) v1 v2) l1
+(<+>) :: Num t => Vec t -> Vec t -> Maybe (Vec t)
+Vec v1 l1 <+> Vec v2 l2 | l1 == l2 = Just (Vec (zipWith (+) v1 v2) l1)
+                        | l1 /= l2 = Nothing
 
 main = print ((Vec [1, 2, 3] 3) <+> (Vec [2, 3, 4] 3))
