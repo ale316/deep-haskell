4b7cb842abe574c342e90dff5cd3e42ff3d233a1
Alessandro Marin <alessandro.m316@gmail.com>
2017-01-30 08:23:21 +0100

    adds scalar addition and multiplication of vectors

--- chapter-2/Vector.hs
+++ chapter-2/Vector.hs
@@ -3,6 +3,14 @@ import Data.List
 -- Can be Vec Int or Vec Float
 data Vec t = Vec [t] Int deriving (Show, Eq, Ord)
 
+-- Adds a scalar to every entry of a vector
+(|+|) :: Num t => Vec t -> t -> Vec t
+Vec v l |+| n = Vec (map (+ n) v) l
+
+-- Multiplies a scalar by every entry of a vector
+(|*|) :: Num t => Vec t -> t -> Vec t
+Vec v l |*| n = Vec (map (* n) v) l
+
 -- Adds two vectors
 -- Note: if the declared dimension of the vectors are different, it returns Nothing
 --       but it doesn't actually check the length of the list.
@@ -10,4 +18,5 @@ data Vec t = Vec [t] Int deriving (Show, Eq, Ord)
 Vec v1 l1 <+> Vec v2 l2 | l1 == l2 = Just (Vec (zipWith (+) v1 v2) l1)
                         | l1 /= l2 = Nothing
 
-main = print ((Vec [1, 2, 3] 3) <+> (Vec [2, 3, 4] 3))
+-- main = print ((Vec [1, 2, 3] 3) <+> (Vec [2, 3, 4] 3))
+main = print ((Vec [1, 2, 3] 3) |*| 5)
